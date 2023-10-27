ffdd2d409c58bc450555c3d52e75cb239355b8f1
Mar <nvmanh25101@gmail.com>
2023-10-27 08:56:53 +0700

    config css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import GlobalStyles from './components/GlobalStyles/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles>
+      <App />
+    </GlobalStyles>
   </React.StrictMode>,
 )
