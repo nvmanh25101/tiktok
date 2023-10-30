0ae748b3fbf97aa76b06e8afac1b29fe442c1e46
Mar <nvmanh25101@gmail.com>
2023-10-30 13:43:50 +0700

    update UI header: search result

---
+++ src/components/AccountItem/index.jsx
@@ -0,0 +1,24 @@
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss"
+import { BsCheckCircleFill } from "react-icons/bs";
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return ( 
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')}
+                src="https://i.pinimg.com/564x/62/90/ab/6290abeb2231d37b7c508eff6f620e91.jpg"
+                alt="avatar" />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>name</span>
+                    <span>< BsCheckCircleFill className={cx('check')} /></span>
+                </p>
+                <span className={cx('username')}>name</span>
+            </div>
+        </div>
+     );
+}
+
+export default AccountItem;
--- src/components/Layouts/components/Header/index.jsx
+++ src/components/Layouts/components/Header/index.jsx
@@ -1,26 +1,60 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
 import { IoMdCloseCircle } from 'react-icons/io';
 import { BiLoaderAlt } from 'react-icons/bi';
 import { BsSearch } from 'react-icons/bs';
 
 import styles from './Header.module.scss'
 import images from '../../../../assets/image';
+import { Wrapper as PopperWrapper } from '../../../Popper';
+import AccountItem from '../../../AccountItem';
 
 const cx = classNames.bind(styles); // dung - cho bien(post-item)
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([])
+        }, 5000)
+    })
+
     return (
         <header className={cx('wrapper')}>
             <img src={images.logo} alt="Tiktok" />
-            <div className={cx("search")}>
-                <input type="text" placeholder='Search' />
-                <button className={cx('search-clear')}>
-                    <IoMdCloseCircle />
-                </button>
-                < BiLoaderAlt className={cx('search-loading')} />
-                <button className={cx('search-btn')}>
-                    < BsSearch />
-                </button>
-            </div>
+                
+            <Tippy 
+                interactive
+                visible={searchResult.length > 0}
+                render={attrs => (
+                    <PopperWrapper>
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <h4 className={cx('search-title')}>
+                                Account
+                            </h4>
+                            < AccountItem />
+                            < AccountItem />
+                            < AccountItem />
+                            < AccountItem />
+                        </div>
+                    </PopperWrapper>
+
+                )}
+            >
+                <div className={cx("search")}>
+                    <input type="text" placeholder='Search' />
+                    <button className={cx('search-clear')}>
+                        <IoMdCloseCircle />
+                    </button>
+                    < BiLoaderAlt className={cx('search-loading')} />
+
+                    <button className={cx('search-btn')}>
+                        < BsSearch />
+                    </button>
+                </div>
+            </Tippy>
+
             <div className={cx('action')}></div>
         </header>
     )
