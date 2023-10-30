15514c494ab4f735751bb267a3c3989918b35397
Mar <nvmanh25101@gmail.com>
2023-10-30 10:16:10 +0700

    update UI header

---
+++ src/assets/image/index.js
@@ -0,0 +1,6 @@
+import logo from './logo.svg'
+const images = {
+    logo: logo,
+}
+
+export default images
--- src/components/Layouts/components/Header/index.jsx
+++ src/components/Layouts/components/Header/index.jsx
@@ -1,9 +1,29 @@
 import classNames from 'classnames/bind';
+import { IoMdCloseCircle } from 'react-icons/io';
+import { BiLoaderAlt } from 'react-icons/bi';
+import { BsSearch } from 'react-icons/bs';
+
 import styles from './Header.module.scss'
+import images from '../../../../assets/image';
 
 const cx = classNames.bind(styles); // dung - cho bien(post-item)
 function Header() {
-    return <header className={cx('wrapper')}></header>;
+    return (
+        <header className={cx('wrapper')}>
+            <img src={images.logo} alt="Tiktok" />
+            <div className={cx("search")}>
+                <input type="text" placeholder='Search' />
+                <button className={cx('search-clear')}>
+                    <IoMdCloseCircle />
+                </button>
+                < BiLoaderAlt className={cx('search-loading')} />
+                <button className={cx('search-btn')}>
+                    < BsSearch />
+                </button>
+            </div>
+            <div className={cx('action')}></div>
+        </header>
+    )
 }
 
 export default Header;
